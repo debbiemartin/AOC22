89c554f95963b5806446b34a10adb27a354058f1
Debbie Martin <dgmartin42@gmail.com>
2022-12-02 19:52:36 +0000

    Day 2 pt 2

--- aoc/src/days/day_2.rs
+++ aoc/src/days/day_2.rs
@@ -2,9 +2,13 @@ use crate::problem::Problem;
 
 pub struct DayTwo {}
 
+fn modulo(val: isize, modulus: isize) -> u8 {
+    (((val % modulus) + modulus) % modulus) as u8
+}
+    
+
 fn comparison_score(opponent: u8, player: u8) -> u32 {
-    // Rust gives remainder like C from % NOT modulus
-    let comparison = (((player as isize - opponent as isize) % 3) + 3) % 3;
+    let comparison = modulo(player as isize - opponent as isize, 3);
     match comparison {
         0 => 3, // draw
         1 => 6, // win
@@ -18,11 +22,29 @@ fn player_score(player: u8) -> u32 {
         1 => 1, 
         2 => 2,
         3 => 3,
-        _ => panic!("Unknown player type")
+        _ => panic!("Unknown player type: {}", player)
     } 
 }
 
-fn parse(input: &str) -> Vec<u32> {  
+fn player_as_action(_opponent: u8, player: &str) -> u8 {
+   match player {
+        "X" => 1,
+        "Y" => 2,
+        "Z" => 3,
+        &_ => panic!("Opponent not recognised")
+    }
+}
+
+fn player_as_result(opponent: u8, player: &str) -> u8 {
+    match player {
+        "X" => modulo(opponent as isize - 2, 3) + 1, // lose
+        "Y" => opponent, // draw
+        "Z" => modulo(opponent as isize, 3) + 1, // win
+        &_ => panic!("Opponent not recognised")
+    }
+}
+
+fn parse<F: Fn(u8, &str) -> u8>(input: &str, player_infer: F) -> Vec<u32> {  
     let mut rounds = Vec::new();
     for line in input.split("\n") {
         if line == "" {
@@ -35,12 +57,7 @@ fn parse(input: &str) -> Vec<u32> {
             "C" => 3,
             &_ => panic!("Opponent not recognised")
         };
-        let player = match opponent_player[1] {
-            "X" => 1,
-            "Y" => 2,
-            "Z" => 3,
-            &_ => panic!("Player not recognised")
-        };
+        let player = player_infer(opponent, opponent_player[1]);
         let round_score: u32 = comparison_score(opponent, player) + player_score(player);
         
         rounds.push(round_score);
@@ -52,13 +69,15 @@ fn parse(input: &str) -> Vec<u32> {
 
 impl Problem for DayTwo {
     fn part_one(&self, input: &str) -> String {
-        let rounds: Vec<u32> = parse(input);
+        let rounds: Vec<u32> = parse(input, player_as_action);
         let total: u32 = rounds.iter().sum();
         format!("Total score: {}", total)
     }
 
-    fn part_two(&self, _input: &str) -> String {
-        format!("Not yet implemented")
+    fn part_two(&self, input: &str) -> String {
+        let rounds: Vec<u32> = parse(input, player_as_result);
+        let total: u32 = rounds.iter().sum();
+        format!("Total score: {}", total)
     }
 }
 
@@ -66,15 +85,27 @@ impl Problem for DayTwo {
 mod tests {
     use super::*;
     #[test]
-    fn find_scores() {
+    fn find_scores_pt1() {
         let input = "A Y
         B X
         C Z";
-        let result: Vec<u32> = parse(input);
+        let result: Vec<u32> = parse(input, player_as_action);
         assert_eq!(result.len(), 3);
         assert_eq!(result[0], 8);
         assert_eq!(result[1], 1);
         assert_eq!(result[2], 6);
+    }
+
+    #[test]
+    fn find_scores_pt2() {
+        let input = "A Y
+        B X
+        C Z";
+        let result: Vec<u32> = parse(input, player_as_result);
+        assert_eq!(result.len(), 3);
+        assert_eq!(result[0], 4);
+        assert_eq!(result[1], 1);
+        assert_eq!(result[2], 7);
     }
 
     #[test]
@@ -84,5 +115,14 @@ mod tests {
         C Z";
         let result = DayTwo{}.part_one(&input);
         assert_eq!(result, "Total score: 15");
+    }
+
+    #[test]
+    fn sum_p2() {
+        let input = "A Y
+        B X
+        C Z";
+        let result = DayTwo{}.part_two(&input);
+        assert_eq!(result, "Total score: 12");
     }
 }
