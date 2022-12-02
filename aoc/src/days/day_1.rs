4068dc7e9aa048548db8a5c2352ddc508c6ac65b
Debbie Martin <dgmartin42@gmail.com>
2022-12-02 17:40:28 +0000

    Refactor into extensible framework

---
+++ aoc/src/days/day_1.rs
@@ -0,0 +1,46 @@
+use crate::problem::Problem;
+
+pub struct DayOne {}
+
+fn parse(input: &str) -> Vec<i32> {    
+    // Consumes the iterator, returns an (Optional) String
+    let mut sum = 0;
+    let mut elf_scores = Vec::new();
+    for line in input.split("\n") {
+        if line == "" {
+            elf_scores.push(sum);
+            sum = 0;
+        } else {
+            sum += line.parse::<i32>().unwrap();
+        }
+    }
+
+    elf_scores
+}
+
+
+impl Problem for DayOne {
+    fn part_one(&self, input: &str) -> String {
+        let elf_scores = parse(input);
+        let max_value = elf_scores.iter().max();
+        match max_value {
+            Some(max) => format!( "Maximum elf score: {}", max ),
+            None      => format!( "No elf totals found" ),
+        }
+    }
+
+    fn part_two(&self, input: &str) -> String {
+        let mut elf_scores = parse(input);
+        // Part 2
+        elf_scores.sort();
+        elf_scores.reverse();
+        elf_scores.truncate(3);
+        let top_3_total: i32 = elf_scores.iter().sum();
+        format!("Sum of top 3 elf scores: {top_3_total}")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*; //@@@ add some tests
+}
---
+++ aoc/src/main.rs
@@ -0,0 +1,58 @@
+pub mod days;
+pub mod problem;
+
+use crate::problem::Problem;
+use crate::days::day_1::DayOne;
+use std::fs;
+use std::env;
+
+fn main() {
+    let day = get_day().unwrap();
+
+    let input_name = format!("./src/inputs/input_{}.txt", day);
+    let input = read_file(input_name);
+    let problem = day_to_problem(day).unwrap();
+    let part_1 = problem.part_one(&input);
+    println!("Part 1 answer: {}", part_1);
+    
+    let part_2 = problem.part_two(&input);
+    println!("Part 2 answer: {}", part_2);
+} 
+
+fn get_day() -> Option<usize> {
+    let args: Vec<String> = env::args().collect();
+
+    let arg = match args.get(1) {
+        Some(val) => val,
+        None => {
+            println!("Not enough arguments provided!");
+            return None;
+        }
+    };
+
+    match arg.parse::<usize>() {
+        Ok(val) => Some(val),
+        Err(e) => {
+            println!("Unable to cast argument {} to usize: {}", arg, e);
+            return None;
+        }
+    }
+}
+    
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_file(filepath: String) -> String {
+    println!("{filepath}");
+    fs::read_to_string(filepath)
+        .expect("Should have been able to read the file")
+}
+
+
+fn day_to_problem(day: usize) -> Option<Box<dyn Problem>> {
+    match day {
+        1 => Some(Box::new(DayOne{})),
+        // ...
+        _ => None
+    }
+}
