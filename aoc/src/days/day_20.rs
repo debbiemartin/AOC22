fe8e8902cb5c25e385800ac3abe630c854af535e
Debbie Martin <dgmartin42@gmail.com>
2022-12-20 18:57:34 +0000

    Day 20 part b

--- aoc/src/days/day_20.rs
+++ aoc/src/days/day_20.rs
@@ -4,53 +4,58 @@ use std::collections::VecDeque;
 pub struct DayTwenty;
 
 struct Cipher {
-    content: VecDeque<(i32, usize)>
+    content: VecDeque<(i64, usize)>,
+    decryption_key: i64
 }
 
 impl Cipher {   
-    fn decrypt(&mut self) {
-        for i in 0..self.content.len() {
-            let index = self.content.iter().position(|&r| r.1 == i).unwrap();
-            let element = self.content.remove(index).unwrap();
-            let new_index = (index as i32 + element.0).rem_euclid(self.content.len() as i32) as usize;
-            self.content.insert(new_index, element)
+    fn decrypt(&mut self, run_count: u32) {
+        for _ in 0..run_count {
+            for i in 0..self.content.len() {
+                let index = self.content.iter().position(|&r| r.1 == i).unwrap();
+                let element = self.content.remove(index).unwrap();
+                let new_index = (index as i64 + element.0).rem_euclid(self.content.len() as i64) as usize;
+                self.content.insert(new_index, element)
+            }
         }
     }
 
-    fn get_sum(&self) -> i32 {
+    fn get_sum(&self) -> i64 {
         let zero_index = self.content.iter().position(|&r| r.0 == 0).unwrap();
         
         let mut sum = 0;
         for index in [1000, 2000, 3000] {
-            let val = self.content.get((zero_index + index) % self.content.len()).unwrap().0;
-            sum += val;
+            sum += self.content.get((zero_index + index) % self.content.len()).unwrap().0;
         }
 
-        sum
+        sum as i64
     }
 
-    pub fn new(input: &str) -> Cipher {
+    pub fn new(input: &str, decryption_key: i64) -> Cipher {
         let mut content = VecDeque::new();
         for (i, numstr) in input.lines().enumerate() {
-            let num: i32 = numstr.parse().unwrap();
-            content.push_back((num, i));
+            let num: i64 = numstr.parse().unwrap();
+            content.push_back(((num * decryption_key), i));
         }
 
-        Cipher { content }
+        Cipher { content, decryption_key }
     }
 }
 
 
 impl Problem for DayTwenty {
     fn part_one(&self, input: &str) -> String {
-        let mut cipher = Cipher::new(&input);
-        cipher.decrypt();
+        let mut cipher = Cipher::new(&input, 1);
+        cipher.decrypt(1);
         let sum = cipher.get_sum();
         format!("Sum: {sum}")
     }
 
-    fn part_two(&self, _input: &str) -> String {
-        format!("Not yet implemented")
+    fn part_two(&self, input: &str) -> String {
+        let mut cipher = Cipher::new(&input, 811589153);
+        cipher.decrypt(10);
+        let sum = cipher.get_sum();
+        format!("Sum: {sum}")
     }
 }
 
@@ -71,5 +76,11 @@ mod tests {
     fn test_cipher_p1() {
         let output = DayTwenty{}.part_one(&INPUT);
         assert_eq!(output, "Sum: 3")
+    }
+
+    #[test]
+    fn test_cipher_p2() {
+        let output = DayTwenty{}.part_two(&INPUT);
+        assert_eq!(output, "Sum: 1623178306")
     }
 }
